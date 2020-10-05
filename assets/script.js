c89df000ec701e3e0976bc74298c32aad5d7fa64
Shuelong Moua <moua_shuelong@yahoo.com>
2020-10-05 14:29:52 -0500

    created html and js files. Added pseudo coding

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+/* 
+button functions:
+onclick search function (Clear previous results)
+generate product from Target and Walmart API and display on the main divs (display product picture, pricing, and rating)
+display 3-5 related products based on rating and pricing of what the user selects
+
+drop down:
+use localstorage to prepend previous search histories 
+search history buttons in drop down should do the same as the search onclick button
